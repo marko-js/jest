228c4ae691a7e37ccfdd71f576dad636e8f01a94
Dylan Piercey <dpiercey@ebay.com>
2019-06-24 12:35:44 -0700

    test: add tests for mocking templates

--- test/browser.test.ts
+++ test/browser.test.ts
@@ -1,9 +1,23 @@
 import Example from "./fixtures/example.marko";
+import Mockable from "./fixtures/mockable.marko";
+
+afterEach(() => {
+  document.body.innerHTML = "";
+});
 
 test("works in the browser", async () => {
   const result = await Example.render({});
   result.appendTo(document.body).getComponent();
   expect(document.body.innerHTML).toMatchInlineSnapshot(
     `"<div>Hello World</div>"`
+  );
+});
+
+jest.mock("./fixtures/mockable.marko");
+test("can be mocked", async () => {
+  const result = await Mockable.render({});
+  result.appendTo(document.body).getComponent();
+  expect(document.body.innerHTML).toMatchInlineSnapshot(
+    `"<div>Hello Mocked</div>"`
   );
 });
--- test/server.test.ts
+++ test/server.test.ts
@@ -1,7 +1,15 @@
 import Example from "./fixtures/example.marko";
+import Mockable from "./fixtures/mockable.marko";
 
 test("server rendering", () => {
   expect(Example.renderSync({}).toString()).toMatchInlineSnapshot(
     `"<div>Hello World</div>"`
+  );
+});
+
+jest.mock("./fixtures/mockable.marko");
+test("can be mocked", async () => {
+  expect(Mockable.renderSync({}).toString()).toMatchInlineSnapshot(
+    `"<div>Hello Mocked</div>"`
   );
 });
