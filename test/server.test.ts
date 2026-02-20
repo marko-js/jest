2f7175c1d7c4dd4a96c48301f1db1e6f8e48fe66
LuLaValva <lukelavalva@gmail.com>
2026-02-20 10:42:36 -0800

    chore: format

--- test/server.test.ts
+++ test/server.test.ts
@@ -4,19 +4,19 @@ import Project from "./fixtures/project/index.marko";
 
 test("server rendering", () => {
   expect(Example.renderSync({}).toString()).toMatchInlineSnapshot(
-    `"<div>Hello World</div>"`
+    `"<div>Hello World</div>"`,
   );
 });
 
 jest.mock("./fixtures/mockable.marko");
 test("can be mocked", async () => {
   expect(Mockable.renderSync({}).toString()).toMatchInlineSnapshot(
-    `"<div>Hello Mocked</div>"`
+    `"<div>Hello Mocked</div>"`,
   );
 });
 
 test("transforms templates in node_modules", () => {
   expect(Project.renderSync({}).toString()).toMatchInlineSnapshot(
-    `"<div class=direct><div class=indirect>Hello World</div></div>"`
+    `"<div class=direct><div class=indirect>Hello World</div></div>"`,
   );
 });
